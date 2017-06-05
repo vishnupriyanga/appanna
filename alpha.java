cd7480c73db7eccf3b9c7c619d3bf73e2d90a813
vishnupriyanga <priyanga.boomika@gmail.com>
2017-06-05 23:47:05 +0530

    Create alpha.java

---
+++ alpha.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.Scanner;
+public class alpha
+{
+public static void main(String args[])
+{
+char a;
+Scanner s=new Scanner(System.in);
+a=new next().charAt(0);
+if(char a=='a'&&char a=='z'||char a=='A'&&char a=='Z')
+{
+System.out.println("alphabet")
+}
+else
+{
+System.out.println("not an alphabet");
+}
+}
+}
