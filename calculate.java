d8f8071a7cce7e76bd6a0bcf8118a85382f92943
vishnupriyanga <priyanga.boomika@gmail.com>
2017-06-05 17:41:57 +0530

    Update calculate.java

--- calculate.java
+++ calculate.java
@@ -4,5 +4,21 @@ public class calculate
 {
 public static void main(String args[])
 {
+int n;
+System.out.println("Enter the number");
 Scanner s=new Scanner(System.in);
-int a
+n=s.nextInt();
+if(n==0)
+{
+System.out.println("zero");
+}
+else if(n>=1)
+{
+System.out.println("positive");
+}
+else
+{
+System.out.println("negative");
+}
+}
+}
