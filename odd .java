b1e12ca144ba4c9b03d3e2d007e2012124d1d832
vishnupriyanga <priyanga.boomika@gmail.com>
2017-06-05 17:52:22 +0530

    Create odd .java

---
+++ odd .java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.Scanner;
+public class odd
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int n;
+n=s.nextInt();
+if(n%2==0)
+{
+System.out.pritln("even");
+}
+else
+{
+System.out.println("odd");
+}
+}
+}
