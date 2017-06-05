b77f3567402df186c91abff66e6d12a8b82ecf4f
vishnupriyanga <priyanga.boomika@gmail.com>
2017-06-05 17:34:58 +0530

    Update calculate.java

--- calculate.java
+++ calculate.java
@@ -5,16 +5,4 @@ public class calculate
 public static void main(String args[])
 {
 Scanner s=new Scanner(System.in);
-int a[]=new int[n];
-int n=s.nextInt();
-for(int i=0;i<n;i++)
-{
-a[i]=s.nextInt();
-}
-int sum=0;
-for(i=0;i<n;i++)
-{
-sum=sum+a[i];
-}
-System.out.println("sum");
-}
+int a
