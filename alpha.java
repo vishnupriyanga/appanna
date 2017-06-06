045900ceed42c3c961710fbf18e568a489a87405
vishnupriyanga <priyanga.boomika@gmail.com>
2017-06-06 18:46:21 +0530

    Update alpha.java

--- alpha.java
+++ alpha.java
@@ -7,7 +7,7 @@ public static void main(String args[])
 char a;
 Scanner s=new Scanner(System.in);
 a=new next().charAt(0);
-if(char a=='a'&&char a=='z'||char a=='A'&&char a=='Z')
+if(char a>='a'&&char a>='z'||char a>='A'&&char a>='Z')
 {
 System.out.println("alphabet")
 }
