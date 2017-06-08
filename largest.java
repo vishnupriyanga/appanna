188a908f10942424ba2839d0a0e9320919824571
vishnupriyanga <priyanga.boomika@gmail.com>
2017-06-08 10:58:31 +0530

    Create largest.java

--- largest.java
+++ largest.java
@@ -8,11 +8,11 @@ Scanner s=new Scanner(System.in);
 int a=s.nextInt();
 int b=s.nextInt();
 int c=s.nextInt();
-if(a>b&&a>c)
+if((a>b)&&(a>c))
 {
 Sytem.out.println(a);
 }
-else if(b>c&&b>a)
+else if((b>c)&&(b>a))
 {
 System.out.println(b);
 }
