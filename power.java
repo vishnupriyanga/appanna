32187a9de1b671102b90717f8ac5d37676ad9fa4
vishnupriyanga <priyanga.boomika@gmail.com>
2017-06-09 15:23:20 +0530

    Create power.java

--- power.java
+++ power.java
@@ -8,6 +8,6 @@ public static void main(String args[])
 Scanner s=new Scanner(System.in);
 int a=s.nextInt();
 int b=s.nextInt();
-int c=Math.pow(a,b);
+int c=(int)Math.pow(a,b);
 System.out.println(c);
 }
