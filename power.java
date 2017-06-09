49970d51a9787bfc4242716ee4b0e998a2eaa3c9
vishnupriyanga <priyanga.boomika@gmail.com>
2017-06-09 15:19:21 +0530

    Create power.java

---
+++ power.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+public class power
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int a=s.nextInt();
+int b=s.nextInt();
+int c=Math.pow(a,b);
+System.out.println(c);
+}
