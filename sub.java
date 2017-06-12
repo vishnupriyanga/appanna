615c0376c7ed5ef208c3bc6c86ad301bebd2b313
vishnupriyanga <priyanga.boomika@gmail.com>
2017-06-12 15:38:56 +0530

    Create sub.java

---
+++ sub.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+public class sub
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int d=s.nextInt();
+String a=s.next();
+int n=a.length();
+char c[]=a.toCharArray[];
+for(int i=0;i<d-1;i++)
+{
+for(int j=i+1;j<n;j++)
+{
+System.out.println(c[i]);
+System.out.println(c[j]);
+}
+}
+}
+}
