36d06b7da94b6c8b2533bef0997105110d164b12
vishnupriyanga <priyanga.boomika@gmail.com>
2017-06-12 14:28:33 +0530

    Create table.java

---
+++ table.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+public class table
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+String a=s.nextLine();
+string b=s.nextLine();
+int a1=Integer.parseInt(a);
+int b1=Integer.parseInt(b);
+int c=a1*b1;
+String c1=String.valueof(c);
+System.out.println(c);
+}
+}
