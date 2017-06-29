37e17bb05684a5cc0d89ae72239ebdc2d10a430a
vishnupriyanga <priyanga.boomika@gmail.com>
2017-06-29 11:06:51 +0530

    Create rev.java

---
+++ rev.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+public class rev
+{
+public static void main(String args[])
+{
+int x;
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+while(n>0)
+{
+x=n%10;
+}
+n=n/10;
+System.out.println(x);
+}
+}
